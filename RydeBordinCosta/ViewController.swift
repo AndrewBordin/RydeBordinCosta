3d92c11d5c866735d338df3fa8d230a6bfba2a28
Andrew <bordinan@sheridancollege.ca>
2018-10-09 09:42:53 -0400

    Changed Label id.

--- RydeBordinCosta/ViewController.swift
+++ RydeBordinCosta/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var txtFromLocation: UITextField!
     @IBOutlet weak var txtToLocation: UITextField!
     
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
